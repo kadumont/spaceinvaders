a3584c256b5ea5e345c328fe7e9a93e0dd9fc3e5
Kaissy Dumont <luck.shang@protonmail.com>
2020-05-04 10:52:30 +0200

    Positionner un nouveau vaisseau

---
+++ src/main/java/fr/unilim/iut/spaceunvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,11 @@
+package fr.unilim.iut.spaceunvaders.utils;
+
+@SuppressWarnings("serial")
+public
+class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
